080cf812ddffcf69e2358b9e5295bf998eddcb21
hec <hec9527@foxmail.com>
2021-02-01 16:50:33 +0800

    webview基础用法

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,35 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { getWebViewContent } from "./util/";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "muse" is now active!');
+  let panel: vscode.WebviewPanel;
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('muse.helloWorld', () => {
-      vscode.window.showInformationMessage('Hello World from muse!');
+    vscode.commands.registerCommand("muse.start", () => {
+      vscode.window.showInformationMessage("Hello World from muse!");
+
+      panel = vscode.window.createWebviewPanel("musePanel", "muse", vscode.ViewColumn.One, {
+        enableScripts: true,
+        retainContextWhenHidden: true,
+      });
+
+      // panel.webview.html = getWebViewContent(context, './view/index.html');
+      panel.webview.html = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>muse</title>
+        </head>
+        <body>
+          <!--  -->
+          <!--  -->
+          hello world
+        </body>
+      </html>
+      `;
     })
   );
 }
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,6 +1,6 @@
-import * as vscode from 'vscode';
-import path from 'path';
-import fs from 'fs';
+import * as vscode from "vscode";
+import path from "path";
+import fs from "fs";
 
 /**
  *  获取view的内容，并且修改其中的链接
@@ -19,25 +19,13 @@ import fs from 'fs';
 //     });
 // }
 
-export function getWebViewContent(
-  context: vscode.ExtensionContext,
-  templatePath: string
-) {
+export function getWebViewContent(context: vscode.ExtensionContext, templatePath: string) {
   const resourcePath = path.join(context.extensionPath, templatePath);
   const dirPath = path.dirname(resourcePath);
-  let html = fs.readFileSync(resourcePath, 'utf-8');
+  let html = fs.readFileSync(resourcePath, "utf-8");
   // vscode不支持直接加载本地资源，需要替换成其专有路径格式，这里只是简单的将样式和JS的路径替换
-  html = html.replace(
-    /(<link.+?href="|<script.+?src="|<img.+?src=")(.+?)"/g,
-    (m: any, $1: string, $2: string) => {
-      return (
-        $1 +
-        vscode.Uri.file(path.resolve(dirPath, $2))
-          .with({ scheme: 'vscode-resource' })
-          .toString() +
-        '"'
-      );
-    }
-  );
+  html = html.replace(/(<link.+?href="|<script.+?src="|<img.+?src=")(.+?)"/g, (m: any, $1: string, $2: string) => {
+    return $1 + vscode.Uri.file(path.resolve(dirPath, $2)).with({ scheme: "vscode-resource" }).toString() + '"';
+  });
   return html;
 }

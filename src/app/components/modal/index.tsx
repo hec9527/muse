0e5a44f8d233d5be76824ad81a9c384e9bdd4488
hec9527 <hec9527@foxmail.com>
2021-07-23 16:47:46 +0800

    添加modal弹窗

--- src/app/components/modal/index.tsx
+++ src/app/components/modal/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import Button from '../button';
+import Button, { ButtonProps } from '../button';
 import './index.less';
 
 interface ModalProps {
   visible: boolean;
   title?: string;
+  okButtonType?: ButtonProps['type'];
+  okButtonText?: string;
+  cancelButtonType?: ButtonProps['type'];
+  cancelButtonText?: string;
+  onOk?: () => void;
+  onCancel?: () => void;
+  extButtons?: React.ReactFragment;
 }
 
-const Modal: React.FC<ModalProps> = ({ visible, title = '', children }) => {
+const Modal: React.FC<ModalProps> = ({
+  visible,
+  title = '确认发布',
+  onOk,
+  onCancel,
+  children,
+  extButtons,
+  okButtonText = '确定',
+  okButtonType = 'primary',
+  cancelButtonText = '取消',
+  cancelButtonType = 'text',
+}) => {
   return visible ? (
     <div className='muse-modal'>
       <div className='muse-modal-mask' />
@@ -15,14 +33,14 @@ const Modal: React.FC<ModalProps> = ({ visible, title = '', children }) => {
         <div className='muse-modal-main'>
           <div className='muse-modal-title'>{title}</div>
           <div className='muse-modal-body'>{children}</div>
-          <div className='muse-title-footer'>
-            <Button type='primary'>取消</Button>
-            <Button type='dashed'>取消</Button>
-            <Button type='text'>取消</Button>
-            <Button type='danger'>取消</Button>
-            <Button type='danger' disable>
-              取消
+          <div className='muse-modal-footer'>
+            <Button type={cancelButtonType} onClick={onCancel}>
+              {cancelButtonText}
             </Button>
+            <Button type={okButtonType} onClick={onOk}>
+              {okButtonText}
+            </Button>
+            {extButtons}
           </div>
         </div>
       </div>

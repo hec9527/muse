c2508ac529a6675bb08a588862b9ee754c4dda30
hec9527 <hec9527@foxmail.com>
2021-07-23 14:10:56 +0800

    修改项目配置

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const babelLoader = {
+  loader: 'babel-loader',
+  options: {
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+  },
+};
+
+/** @type {import("webpack").Configuration} */
+const config = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/app/index.tsx'),
+
+  target: 'node',
+
+  output: {
+    path: path.resolve(__dirname, './dist/'),
+    filename: 'main.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [babelLoader],
+      },
+      {
+        test: /\.tsx?$/,
+        use: [babelLoader, 'ts-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.tx', '.tsx', '.js', '.less'],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './1.html'),
+    }),
+  ],
+};
+
+module.exports = config;

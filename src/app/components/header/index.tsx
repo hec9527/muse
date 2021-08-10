e1c700143ddfe934d7096b123774e1f07c8ab985
hec9527 <hec9527@foxmail.com>
2021-08-10 20:08:29 +0800

    添加顶部按钮

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Button from '../button';
 import './index.less';
 
 const title = 'rongshu';
 const branch = '6.3.0';
 
 const Header: React.FC = () => {
+  const handleCheckBranchClick = () => {
+    console.log('check branch');
+  };
+
+  const handlePublishClick = () => {
+    console.log('publish code');
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Enter') {
+        handlePublishClick();
+      }
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => document.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
   return (
     <div className='header-container'>
       <div className='project-title'>
-        <span>{title}</span>
+        <span>{title.toUpperCase()}</span>
         <span className='divider'>/</span>
         <span>{branch}</span>
+      </div>
+
+      <div className='muse-btn-group'>
+        <Button onClick={handleCheckBranchClick}>分支</Button>
+        <Button onClick={handlePublishClick}>发布</Button>
       </div>
     </div>
   );

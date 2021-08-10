7ac2324cd17bfa9eff870eebf5377000f1ac03df
hec9527 <hec9527@foxmail.com>
2021-08-10 14:46:47 +0800

    删除侧边栏

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,7 +7,6 @@ import thunk from 'redux-thunk';
 import Header from './components/header';
 import Modal from './components/modal';
 import ServerEnv from './components/server-env';
-import SideBar from './components/sider-bar';
 
 import { reducer } from './store/reducer';
 import './index.less';
@@ -19,7 +18,6 @@ const App: React.FC = () => {
     <Provider store={store}>
       <div id='muse-container'>
         <Header />
-        <SideBar />
         <div className='muse-body'>
           <ServerEnv />
         </div>

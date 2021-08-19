bbbb5aa9ff12e3e4c07f0c23385537a9beac14a8
hec9527 <hec9527@foxmail.com>
2021-08-19 15:22:23 +0800

    添加关闭弹窗快捷键

--- src/app/components/message-handler/index.tsx
+++ src/app/components/message-handler/index.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../../store/reducer';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, AppState } from '../../store/reducer';
 import * as Types from '../../../index.d';
 
-// 空组件， 使用hooks 接受并且处理来自插件的消息
+// 空组件， 使用 hooks 接收并且处理来自插件的消息
 const MessageHandler: React.FC = () => {
+  const state = useSelector((state: AppState) => state);
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    const handler = (e: { data: Types.IExtensionMessage }) => {
+    const handlerMessage = (e: { data: Types.IExtensionMessage }) => {
       const data = e.data;
       switch (data.cmd) {
         case 'UPDATE_ENV_INFO':
@@ -35,14 +36,25 @@ const MessageHandler: React.FC = () => {
       }
     };
 
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape' || e.keyCode === 27) {
+        dispatch({ type: 'UPDATE_PUBLISH_MODAL_VISIBLE', payload: false });
+        dispatch({ type: 'UPDATE_SEARCH_CODE_BRANCH_MODAL_VISIBLE', payload: false });
+      }
+    };
+
     // 初始化
     dispatch({ type: 'POST_MESSAGE_TO_EXTENSION', payload: { cmd: 'GET_ENV_INFO' } });
     dispatch({ type: 'POST_MESSAGE_TO_EXTENSION', payload: { cmd: 'GET_PAGE_INFO' } });
     dispatch({ type: 'POST_MESSAGE_TO_EXTENSION', payload: { cmd: 'GET_PROJECT_INFO' } });
 
-    window.addEventListener('message', handler);
-    () => window.removeEventListener('message', handler);
-  }, []);
+    window.addEventListener('message', handlerMessage);
+    window.addEventListener('keydown', handleKeyDown);
+    () => {
+      window.removeEventListener('message', handlerMessage);
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [state.publishModalVisible, state.searchCodeBranchModakVisible]);
 
   return null;
 };

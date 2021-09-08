312ba9cad957e5e7152b28501ca47764b94ceab0
hec9527 <hec9527@foxmail.com>
2021-09-08 11:09:10 +0800

    删除shelljs,使用npm变量实现自动打包并且拼接版本号

--- script/install.js
+++
@@ -1,6 +0,0 @@
-const shelljs = require('shelljs');
-const { version } = require('../package.json');
-
-const name = `muse-${version}.vsix`;
-
-shelljs.exec(`vsce package -o ${name} && code --install-extension ${name}`);
